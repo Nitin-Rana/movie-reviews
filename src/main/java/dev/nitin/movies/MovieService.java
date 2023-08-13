c589ca23cc45ad5d222eb58355168c018b26b067
Nitin Rana <ntnrana2011@gmail.com>
2023-08-13 16:03:00 +0530

    Backend code pushed

---
+++ src/main/java/dev/nitin/movies/MovieService.java
@@ -0,0 +1,22 @@
+package dev.nitin.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> aLlMovies(){
+        System.out.println(movieRepository.findAll().toString());
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+}
