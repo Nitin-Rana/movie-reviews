caba3981cbf15222ad6e361c2477b513db5be47b
Nitin Rana <ntnrana2011@gmail.com>
2023-08-13 16:45:45 +0530

    bugs removed

--- src/main/java/dev/nitin/movies/Movie.java
+++ src/main/java/dev/nitin/movies/Movie.java
@@ -9,7 +9,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
-
 @Document(collection = "movies")
 @Data
 @AllArgsConstructor
